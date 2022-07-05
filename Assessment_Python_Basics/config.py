515221ba3a0198674bb0d8974adfc28be6e9b63f
Paul Polsinelli <paul.polsinelli@gmail.com>
2022-07-05 08:17:00 -0500

    initial python basics assessment files

---
+++ Assessment_Python_Basics/config.py
@@ -0,0 +1,23 @@
+# Dictionary file location, change if needed.
+dictionaryloc = 'dictionary.txt'
+
+# Turn text file location, change if needed.
+turntextloc = 'turntext.txt'
+
+# Wheel contents for file location, change if needed.
+wheeltextloc = 'wheeldata.txt'
+
+# End of Round Status status file, change if needed.
+roundstatusloc = 'roundstatus.txt'
+
+# number of rounds in a game
+maxrounds = 3
+
+# Vowel cost
+vowelcost = 250
+
+# final prize, you fill this in
+finalprize = 10000
+
+# final round, change if needed.
+finalRoundTextLoc = 'finalround.txt'
